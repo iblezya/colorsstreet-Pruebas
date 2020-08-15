82e5a1762de1accd41c8c2a6c78af20b2aa19125
Gerson Salas <iblezya@gmail.com>
2020-08-15 00:02:41 -0500

    Beta 1.01

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Layout } from "../components"
 
-const Page404 = () => {
+const NotFoundPage = () => {
   return (
     <Layout>
-      <h1>Hi people</h1>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
   )
 }
-export default Page404
+export default NotFoundPage
